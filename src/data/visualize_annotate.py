02fb1c3460764f3ebd08a401a033111f96d80aa0
ta-brook <tanawat.jrw@gmail.com>
2022-03-29 17:34:26 +0700

    #6 get viz_img from label me

---
+++ src/data/visualize_annotate.py
@@ -0,0 +1,33 @@
+import os
+
+annoation = input(f'annotation path (e.g. outcrop, seismic): ')
+
+data_path = 'data/annotation/'
+data_annotated = data_path + f'{annoation}/json/'
+save_labelme_directory = data_path + f'{annoation}/outcrop_visualized/'
+save_viz_img = data_path + f'{annoation}/viz_img/'
+
+# TODO mkdir 
+if not os.path.exists(save_labelme_directory):
+    os.makedirs(save_labelme_directory)
+if not os.path.exists(save_viz_img):
+    os.makedirs(save_viz_img)
+
+# TODO access to annotation
+ls_data_path = [file for file in os.listdir(data_annotated) if file.endswith('.json')]
+
+# TODO visualize and store data
+try:
+    save_path = []
+    for idx, file_name in enumerate(ls_data_path):
+        os.system(f'labelme_json_to_dataset {data_annotated + file_name} -o {save_labelme_directory + file_name[:-5]}')
+        os.replace(f'{save_labelme_directory + file_name[:-5]}/label_viz.png', f'{save_viz_img}/{file_name}.png')
+        print(file_name)
+
+except AssertionError as error:
+    print(error)
+
+print(ls_data_path)
+
+# f = '001_fault_data.json'
+# print(f[:-5])
